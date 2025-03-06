70863986b1b454ecc528e66268c5536e9f3866a3
Lance Valle <vallelance8@gmail.com>
2025-03-06 22:02:47 +0800

    Update Next.js config to support server-side rendering

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true,
+    domains: ['localhost'],
   },
   basePath: process.env.NODE_ENV === 'production' ? '/pos-system' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/pos-system/' : '',
 }
 
 module.exports = nextConfig 
