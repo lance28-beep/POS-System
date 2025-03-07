ed6471c384f21bdcdc51e8d22cbe28107a565055
Lance Valle <vallelance8@gmail.com>
2025-03-07 11:14:38 +0800

    Update Next.js config for dynamic API routes

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
+  experimental: {
+    serverActions: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 }
 
 module.exports = nextConfig 
